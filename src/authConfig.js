e5fd698bec61dec38853a529e1aef9e17306c5c3
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-21 16:16:04 +0300

    changed keycloak

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,16 +1,17 @@
 // export const authConfig = {
-//   clientId: 'oauth2-pkce-client',
-//   authorizationEndpoint: `/realms/cloud-realm/protocol/openid-connect/auth`,
-//   tokenEndpoint: `/realms/cloud-realm/protocol/openid-connect/token`,
-//   redirectUri: window.location.origin,
-//   scope: 'openid profile email offline_access',
-// }
+//     clientId: 'gateway-client',
+//     authorizationEndpoint: `/realms/cloud-realm/protocol/openid-connect/auth`,
+//     tokenEndpoint: `/realms/cloud-realm/protocol/openid-connect/token`,
+//     redirectUri: 'https://34.116.235.108',
+//     scope: 'openid profile email offline_access',
+//     onRefreshTokenExpire: (event) => event.logIn(),
+//   }
 
 export const authConfig = {
-    clientId: 'gateway-client',
-    authorizationEndpoint: `/realms/cloud-realm/protocol/openid-connect/auth`,
-    tokenEndpoint: `/realms/cloud-realm/protocol/openid-connect/token`,
-    redirectUri: 'https://34.116.235.108',
-    scope: 'openid profile email offline_access',
-    onRefreshTokenExpire: (event) => event.logIn(),
-  }
+  clientId: 'frontend-client',
+  authorizationEndpoint: 'https://34.116.235.108/realms/cloud-realm/protocol/openid-connect/auth',
+  tokenEndpoint: 'https://34.116.235.108/realms/cloud-realm/protocol/openid-connect/token',
+  redirectUri: window.location.origin,
+  scope: 'openid profile email',
+  onRefreshTokenExpire: (event) => event.logIn(),
+};
