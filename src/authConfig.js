43402c5d25a035833318086cab93c5f56bf941d8
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-21 12:40:49 +0300

    changed url

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,18 @@
+const BASE_URL = "/api"
+
 export const authConfig = {
-    clientId: 'oauth2-pkce-client',
-    authorizationEndpoint: 'http://localhost:8181/realms/is-order-processing/protocol/openid-connect/auth',
-    tokenEndpoint: 'http://localhost:8181/realms/is-order-processing/protocol/openid-connect/token',
-    redirectUri: 'http://localhost:3000',
-    scope: 'openid profile email offline_access',
-    onRefreshTokenExpire: (event) => event.logIn(),
-  }
+  clientId: 'oauth2-pkce-client',
+  authorizationEndpoint: `${BASE_URL}/realms/cloud-realm/protocol/openid-connect/auth`,
+  tokenEndpoint: `${BASE_URL}/realms/cloud-realm/protocol/openid-connect/token`,
+  redirectUri: window.location.origin,
+  scope: 'openid profile email offline_access',
+}
+
+// export const authConfig = {
+//     clientId: 'oauth2-pkce-client',
+//     authorizationEndpoint: 'http://localhost:8181/realms/is-order-processing/protocol/openid-connect/auth',
+//     tokenEndpoint: 'http://localhost:8181/realms/is-order-processing/protocol/openid-connect/token',
+//     redirectUri: 'http://localhost:3000',
+//     scope: 'openid profile email offline_access',
+//     onRefreshTokenExpire: (event) => event.logIn(),
+//   }
--- src/shared/hooks/useCategories.js
+++ src/shared/hooks/useCategories.js
@@ -4,7 +4,7 @@ export const useCategories = () => {
   const [categories, setCategories] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("http://localhost:8080/categories")
+    fetch("/api/categories")
       .then(res => res.json())
       .then(data => {
         setCategories(Array.isArray(data) ? data : []);
