35d618986955af777c0a2588d0b8ff6e2b3be660
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-21 12:24:51 +0300

    changed path

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'http://gateway-service:8080/api';
+// const API_URL = window.location.origin + '/api';
+const API_URL = '/api';
 
 const api = axios.create({
     baseURL:API_URL
