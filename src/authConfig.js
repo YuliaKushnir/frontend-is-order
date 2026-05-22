4c34f4c857db68ecb78c6325678d37938b9edb59
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-22 11:29:49 +0300

    temp

--- src/authConfig.js
+++ src/authConfig.js
@@ -11,7 +11,7 @@ export const authConfig = {
   clientId: 'gateway-client',
   authorizationEndpoint: 'https://34.116.235.108/realms/cloud-realm/protocol/openid-connect/auth',
   tokenEndpoint: 'https://34.116.235.108/realms/cloud-realm/protocol/openid-connect/token',
-  redirectUri: window.location.origin,
+  redirectUri: "https://34.116.235.108",
   scope: 'openid profile email',
   onRefreshTokenExpire: (event) => event.logIn(),
 };
