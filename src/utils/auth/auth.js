d6b92aeb2ad83b34afed9feb82e223696de672e1
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-22 13:27:36 +0300

    temp

--- src/utils/auth/auth.js
+++ src/utils/auth/auth.js
@@ -1,20 +1,16 @@
 export const getRoles = (tokenData) => {
-  const roles = tokenData?.realm_access?.roles || [];
-
-  console.log("ROLES =", roles);
-
-  return roles;
+  return tokenData?.resource_access?.["order-processing"]?.roles || [];
 };
 
-export const hasRole = (tokenData, role) => {
-  console.log("tokenData =", tokenData);
+// export const getRoles = (tokenData) => {
+//   return tokenData?.realm_access?.roles || [];
+// };
 
+export const hasRole = (tokenData, role) => {
   return getRoles(tokenData).includes(role);
 };
 
 export const hasAnyRole = (tokenData, roles) => {
-  console.log("tokenData =", tokenData);
-
   const userRoles = getRoles(tokenData);
 
   return roles.some(role =>
