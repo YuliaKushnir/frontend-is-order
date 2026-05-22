eba1952ab407bc0fd2c5a7ee4776554c8ac9e8c5
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-22 13:20:23 +0300

    changed keycloak

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,45 +1,100 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const storedUser = JSON.parse(localStorage.getItem('user'));
+
 const authSlice = createSlice({
   name: 'auth',
-  initialState : {
-    user: JSON.parse(localStorage.getItem('user')) || null,
-    token: localStorage.getItem('token') || null,
-    userId: localStorage.getItem('userId') | null
-  },
-  reducers: {
-    // setCredentials: (state, action) => {
-    //   state.user = action.payload.user;
-    //   state.token = action.payload.token;
-    //   state.userId = action.payload.user.sub;
 
-    //   localStorage.setItem('token', action.payload.token);
-    //   localStorage.setItem('user', JSON.stringify(action.payload.user));
-    //   localStorage.setItem('userId', action.payload.user.sub);
-    // },
+  initialState: {
+    user: storedUser || null,
+    token: localStorage.getItem('token') || null,
+    userId: localStorage.getItem('userId') || null,
+    roles: JSON.parse(localStorage.getItem('roles')) || []
+  },
+
+  reducers: {
     setCredentials: (state, action) => {
+
       const user = action.payload.user;
+      const token = action.payload.token;
+
+      const roles = user?.realm_access?.roles || [];
 
       state.user = user;
-      state.token = action.payload.token;
+      state.token = token;
       state.userId = user?.sub || null;
+      state.roles = roles;
 
-      localStorage.setItem('token', action.payload.token);
+      localStorage.setItem('token', token);
       localStorage.setItem('user', JSON.stringify(user));
       localStorage.setItem('userId', user?.sub || '');
-      console.log("userId", user?.sub, " user ", user)
+      localStorage.setItem('roles', JSON.stringify(roles));
+
+      console.log("USER:", user);
+      console.log("USER ID:", user?.sub);
+      console.log("ROLES:", roles);
     },
+
     logout: (state) => {
       state.user = null;
       state.token = null;
       state.userId = null;
-      
+      state.roles = [];
+
       localStorage.removeItem('token');
       localStorage.removeItem('user');
       localStorage.removeItem('userId');
+      localStorage.removeItem('roles');
     },
   },
 });
 
 export const { setCredentials, logout } = authSlice.actions;
+
 export default authSlice.reducer;
+
+// import { createSlice } from '@reduxjs/toolkit'
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState : {
+//     user: JSON.parse(localStorage.getItem('user')) || null,
+//     token: localStorage.getItem('token') || null,
+//     userId: localStorage.getItem('userId') | null
+//   },
+//   reducers: {
+//     // setCredentials: (state, action) => {
+//     //   state.user = action.payload.user;
+//     //   state.token = action.payload.token;
+//     //   state.userId = action.payload.user.sub;
+
+//     //   localStorage.setItem('token', action.payload.token);
+//     //   localStorage.setItem('user', JSON.stringify(action.payload.user));
+//     //   localStorage.setItem('userId', action.payload.user.sub);
+//     // },
+//     setCredentials: (state, action) => {
+//       const user = action.payload.user;
+
+//       state.user = user;
+//       state.token = action.payload.token;
+//       state.userId = user?.sub || null;
+
+//       localStorage.setItem('token', action.payload.token);
+//       localStorage.setItem('user', JSON.stringify(user));
+//       localStorage.setItem('userId', user?.sub || '');
+//       console.log("userId", user?.sub, " user ", user)
+//     },
+//     logout: (state) => {
+//       state.user = null;
+//       state.token = null;
+//       state.userId = null;
+      
+//       localStorage.removeItem('token');
+//       localStorage.removeItem('user');
+//       localStorage.removeItem('userId');
+//     },
+//   },
+// });
+
+// export const { setCredentials, logout } = authSlice.actions;
+// export default authSlice.reducer;
--- src/utils/auth/auth.js
+++ src/utils/auth/auth.js
@@ -1,20 +1,19 @@
 export const getRoles = (tokenData) => {
-  return tokenData?.resource_access?.["order-processing"]?.roles || [];
+  const roles = tokenData?.realm_access?.roles || [];
+
+  console.log("ROLES =", roles);
+
+  return roles;
 };
 
-// export const getRoles = (tokenData) => {
-//   console.log("ROLE = ", tokenData?.realm_access?.roles || [])
-//   return tokenData?.realm_access?.roles || [];
-// };
-
 export const hasRole = (tokenData, role) => {
-  console.log("tokenData = ", tokenData)
+  console.log("tokenData =", tokenData);
 
   return getRoles(tokenData).includes(role);
 };
 
 export const hasAnyRole = (tokenData, roles) => {
-  console.log("tokenData = ", tokenData)
+  console.log("tokenData =", tokenData);
 
   const userRoles = getRoles(tokenData);
 
